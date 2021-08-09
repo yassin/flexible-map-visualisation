1b3037668167073680d2dd33d4fcb4e5bebdafbb
Yassin Bougrine <yassin.bougrine@creos.net>
2021-08-09 13:48:42 +0200

    stable version of maps

--- src/app/google-maps-demo/google-maps-demo.component.ts
+++ src/app/google-maps-demo/google-maps-demo.component.ts
@@ -1,24 +1,27 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
+import {Component, OnInit, ViewChild} from '@angular/core'
+import {GoogleMap, MapInfoWindow, MapMarker} from '@angular/google-maps'
+
 
 @Component({
   selector: 'google-maps-demo',
   templateUrl: './google-maps-demo.component.html',
 })
 export class GoogleMapsDemoComponent implements OnInit {
+  @ViewChild(GoogleMap, {static: false}) map: GoogleMap
+  @ViewChild(MapInfoWindow, {static: false}) info: MapInfoWindow
 
   zoom = 12
-  center!: google.maps.LatLngLiteral;
+  center: google.maps.LatLngLiteral
   options: google.maps.MapOptions = {
-    mapTypeId: 'hybrid',
     zoomControl: false,
     scrollwheel: false,
     disableDoubleClickZoom: true,
+    mapTypeId: 'hybrid',
     maxZoom: 15,
     minZoom: 8,
   }
+  markers: Array<any> = []
+  infoContent = ''
 
   ngOnInit() {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -30,12 +33,48 @@ export class GoogleMapsDemoComponent implements OnInit {
   }
 
   zoomIn() {
-    // @ts-ignore
-    if (this.zoom < this.options.maxZoom) this.zoom++
+    this.zoom++
   }
 
   zoomOut() {
-    // @ts-ignore
-    if (this.zoom > this.options.minZoom) this.zoom--
+    this.zoom--
+  }
+
+  click(event: google.maps.MapMouseEvent) {
+    // Close the current InfoWindow.
+    //this.info.close();
+    console.log(JSON.stringify(event.latLng.toJSON(), null, 2))
+    //this.info.open(map);
+  }
+
+  logCenter() {
+    console.log(JSON.stringify(this.map.getCenter()))
+  }
+
+  addMarker() {
+
+
+    this.markers.push({
+      position: {
+        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
+        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
+      },
+      label: {
+        // @ts-ignore
+        color: '#' + parseInt(Math.random() * 0xffffff).toString(16),
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      info: 'Marker info ' + (this.markers.length + 1),
+      //options: {
+        //animation: google.maps.Animation.BOUNCE,
+      //},
+    })
+  }
+
+  openInfo(marker: MapMarker,
+           content:any) {
+    this.infoContent = content
+    this.info.open(marker)
   }
 }
