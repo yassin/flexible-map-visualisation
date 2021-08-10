e13a883c706683b14d2fad3caa09331f4f16d89c
Yassin Bougrine <yassin.bougrine@creos.net>
2021-08-10 14:13:02 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {GoogleMapsDemoModule} from './google-maps-demo/google-maps-demo.module'
+import { GoogleMapsModule } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import {GoogleMapsDemoComponent} from "./google-maps-demo/google-maps-demo.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapsDemoComponent
   ],
   imports: [
     BrowserModule,
-    GoogleMapsDemoModule
+    CommonModule,
+    GoogleMapsModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
